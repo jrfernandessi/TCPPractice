8caf07285d1b8e7f8b2fb7b171536374c4ada13e
Junior Fernandes <jrfernandessi@users.noreply.github.com>
2019-02-21 19:49:04 -0300

    testing connection TCP with socket

---
+++ src/TCPServer.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+    public static void main(String x[]) throws Exception{
+        String clientSentence;
+        String capitalizedSentence;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+        while(true){
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(
+                new InputStreamReader(
+                        connectionSocket.getInputStream()
+                )
+            );
+            DataOutputStream outToClient = new DataOutputStream(
+                    connectionSocket.getOutputStream()
+            );
+            clientSentence = inFromClient.readLine();
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+        }
+    }
+}
