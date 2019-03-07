98d2aadb59729b7e5fc6498bab8859b5e8eeda53
Junior Fernandes <jrfernandessi@users.noreply.github.com>
2019-03-07 15:28:35 -0300

    tratando exception com server parado

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Scanner;
@@ -8,20 +7,24 @@ public class TCPClient {
     public static void main(String x[]) throws Exception{
 
         String outputServer;
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.println("digite o host para conexão");
+            String host = input.next();
+            Socket clientSocket = new Socket(host, 8000);
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("digite o host para conexão");
-        String host = input.next();
-        Socket clientSocket = new Socket(host, 8000);
 
-
-        BufferedReader inFromServer = new BufferedReader(
-                new InputStreamReader(
-                        clientSocket.getInputStream()
-                )
-        );
-        outputServer = inFromServer.readLine();
-        System.out.println("conectou no servidor: "+outputServer);
-        clientSocket.close();
+            BufferedReader inFromServer = new BufferedReader(
+                    new InputStreamReader(
+                            clientSocket.getInputStream()
+                    )
+            );
+            outputServer = inFromServer.readLine();
+            System.out.println("conectou no servidor: " + outputServer);
+            clientSocket.close();
+        }
+        catch (Exception e){
+            System.out.println("não conseguiu conectar");
+        }
     }
 }
